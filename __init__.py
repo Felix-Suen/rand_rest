cc7a4eed3c8e6c370b4e6cc5109ed6898acd2630
Felix <felix.suen2000@gmail.com>
2019-03-30 11:03:04 +0800

    Initialize

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from random import choice
+
+from flask import (Flask, render_template, request, redirect, url_for)
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/', methods=('GET', 'POST'))
+    def hello():
+        if request.method == 'POST':
+            return redirect(url_for('rest'))
+        return render_template('start.html')
+
+    @app.route('/rest', methods=('GET', 'POST'))
+    def rest():
+        rest_lst = ['Mcdonalds', 'KFC', 'BurgerKing', 'Subway', 'Pizza Hut']
+        if request.method == 'POST':
+            return redirect(url_for('rest'))
+        random = choice(rest_lst)
+        return render_template('rest.html', random=random)
+
+    return app
+
