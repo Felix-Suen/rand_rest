8f76b540f2a246919dd9ecfa6ed3c20d2cc029dc
Felix <felix.suen2000@gmail.com>
2019-03-30 14:06:03 +0800

    delete init

--- flask-backend/deploy.py
+++ flask-backend/deploy.py
@@ -1,22 +1,19 @@
-import flask
-from flask import Flask, render_template
+
+from random import choice
+
+from flask import Flask, render_template, redirect, request, url_for
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
-def index():
-    return render_template('index.html', token="Testing")
-
-@app.route('/rest')
-def rest():
+def restaurant():
     rest_lst = ['Mcdonalds', 'KFC', 'BurgerKing', 'Subway', 'Pizza Hut']
     if request.method == 'POST':
-        return redirect(url_for('rest'))
+        return redirect(url_for('restaurant'))
     random = choice(rest_lst)
-    return random
+    return render_template('index.html', token=random)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
-
